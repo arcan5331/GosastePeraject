9ac583ba8d16343ccd017e5332d348a8e958d28d
Aligh <alighaforian@yahoo.com>
2023-01-18 00:18:13 +0330

    Second Form and Starling Number
    Added Second Form of Balls And Boxes Problems.

--- Program.cs
+++ Program.cs
@@ -7,11 +7,33 @@ namespace GosastePeraject
         static void Main(string[] args)
         {
             Console.WriteLine("Enter two int.");
-
+            var n = Convert.ToInt32(Console.ReadLine());
+            var k = Convert.ToInt32(Console.ReadLine());
+            var c = StarlingNumber(n,k);
+            System.Console.WriteLine($"Starling number = {c}");
+            System.Console.WriteLine($"Output = {SecendForm(n,k)}");
         }
 
-
+        static Int64 StarlingNumber(int n,int k)
+        {
+            Int64 output = 0;
+            for (int i = 0; i < k; i++)
+            {
+                output += Convert.ToInt64(Math.Pow(k-i,n)*Math.Pow(-1,i));
+            }
+            return output;
         }
+
+        static Int64 SecendForm(int n,int k)
+        {
+            Int64 output = 1;
+            for (int i = k; i > 0; i--)
+            {
+                output *= i;
+            }
+            return output * StarlingNumber(n,k);
+        }
+    }
 
     
 }
