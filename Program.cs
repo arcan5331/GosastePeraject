bd16e7db0d7f1f4a9a19a6e5726cec5ebf20d4ff
Aligh <alighaforian@yahoo.com>
2023-01-18 00:33:33 +0330

    Corrected the Stirling Number
    Corrected Spelling off Stirling Number.
    Corrected the miss understanding of problem.

--- Program.cs
+++ Program.cs
@@ -9,19 +9,24 @@ namespace GosastePeraject
             Console.WriteLine("Enter two int.");
             var n = Convert.ToInt32(Console.ReadLine());
             var k = Convert.ToInt32(Console.ReadLine());
-            var c = StarlingNumber(n,k);
+            var c = StarlingNumber(n, k);
             System.Console.WriteLine($"Starling number = {c}");
-            System.Console.WriteLine($"Output = {SecendForm(n,k)}");
+            System.Console.WriteLine($"Output = {SecendForm(n, k)}");
         }
 
-        static Int64 StarlingNumber(int n,int k)
+        static Int64 StarlingNumber(int n, int k)
         {
             Int64 output = 0;
             for (int i = 0; i < k; i++)
             {
-                output += Convert.ToInt64(Math.Pow(k-i,n)*Math.Pow(-1,i));
+                output += Convert.ToInt64(Math.Pow(k - i, n) * Math.Pow(-1, i));
             }
-            return output;
+            Int64 temp = 1;
+            for (int i = k; i > 0; i--)
+            {
+                temp *= i;
+            }
+            return output/temp;
         }
 
         static Int64 SecendForm(int n,int k)
@@ -35,5 +40,5 @@ namespace GosastePeraject
         }
     }
 
-    
+
 }
