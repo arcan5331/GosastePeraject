b54ca34090eb13b9bd4942c25d071eb0d89d2c9d
Aligh <alighaforian@yahoo.com>
2023-01-17 23:12:48 +0330

    removed Chose

--- Program.cs
+++ Program.cs
@@ -8,38 +8,10 @@ namespace GosastePeraject
         {
             Console.WriteLine("Enter two int.");
 
-            var a = Convert.ToInt32(Console.ReadLine());
-            var b = Convert.ToInt32(Console.ReadLine());
-
-            var c = C(a, b);
-            Console.WriteLine($"c = {c}\n");
         }
 
-        static int C(int n, int k)
-        {
-
-            if (n == k)
-            {
-                return 1;
-            } 
-            else if (n == 1 && k == 1)
-            {
-                return 1;
-            }
-            else if (n == 0 && k == 0)
-            {
-                return 1;
-            }
-            else if (k == 0)
-            {
-                return 1;
-            }
-            else
-            {
-                return (C(n - 1, k - 1) + C(n - 1, k));
-            }
 
         }
 
-    }
+    
 }
